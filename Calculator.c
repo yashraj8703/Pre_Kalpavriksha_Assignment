4e15f3da175267bd6dd425ab71176effd0e5af5c
yashraj8703 <rajyash8703@gmail.com>
2024-12-21 11:44:15 +0530

    Updated Calculator.c

--- Calculator.c
+++ Calculator.c
@@ -5,6 +5,23 @@
 
 #define VALID_SIZE 100
 
+void remove_newline(char *str) {
+    for (int i = 0; i < VALID_SIZE; i++) {
+        if (str[i] == '\n' || str[i] == '\r') {
+            str[i] = '\0';
+            break;
+        }
+    }
+}
+
+int get_string_length(char *str) {
+    int length = 0;
+    while (str[length] != '\0') {
+        length++;
+    }
+    return length;
+}
+
 int calculate(int num1, int num2, char op) {
     if (op == '+') {
         return num1 + num2;
@@ -24,13 +41,72 @@ int calculate(int num1, int num2, char op) {
     }
 }
 
+int precedence(char op) {
+    if (op == '*' || op == '/') {
+        return 2;
+    } else if (op == '+' || op == '-') {
+        return 1;
+    }
+    return 0;
+}
+
+void remove_spaces(char *str) {
+    int len = get_string_length(str);
+    int newIndex = 0;
+
+    for (int i = 0; i < len; i++) {
+        if (isspace(str[i])) {
+            continue;  
+        }
+        str[newIndex++] = str[i];  
+    }
+    str[newIndex] = '\0';  
+}
+
+int validate_expression(char *str) {
+    
+    if (get_string_length(str) == 0) {
+        printf("Error: Invalid expression.\n");
+        return 0;
+    }
+
+    int lastCharWasOperator = 0;
+    for (int i = 0; i < get_string_length(str); i++) {
+        if (!isdigit(str[i]) && str[i] != '+' && 
+            str[i] != '-' && str[i] != '*' && str[i] != '/' ) {
+            printf("Error: Invalid character '%c'.\n", str[i]);
+            return 0;  
+        }
+
+       
+        if ((str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/') && lastCharWasOperator) {
+            printf("Error: Invalid expression (consecutive operators).\n");
+            return 0;  
+        }
+
+       
+        lastCharWasOperator = (str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/');
+    }
+
+  
+    int lastIndex = get_string_length(str) - 1;
+    if (str[lastIndex] == '+' || str[lastIndex] == '-' || 
+        str[lastIndex] == '*' || str[lastIndex] == '/') {
+        str[lastIndex] = '\0';  
+    }
+
+    return 1; 
+}
+
 int calculator(char* validUserInputs) {
     int len = strlen(validUserInputs);
     int i = 0;
     int values[VALID_SIZE];
     char operators[VALID_SIZE];
     int valuesTop = -1, operatorsTop = -1;
+
     while (i < len) {
+        
         if (isdigit(validUserInputs[i])) {
             int num = 0;
             while (i < len && isdigit(validUserInputs[i])) {
@@ -39,18 +115,14 @@ int calculator(char* validUserInputs) {
             }
             values[++valuesTop] = num;
         }
-        else if (validUserInputs[i] == '*' || validUserInputs[i] == '/') {
-            char op = validUserInputs[i];
-            i++; 
-            int num = 0;
-            while (i < len && isdigit(validUserInputs[i])) {
-                num = num * 10 + (validUserInputs[i] - '0');
-                i++;
+        
+        else if (validUserInputs[i] == '+' || validUserInputs[i] == '-' || validUserInputs[i] == '*' || validUserInputs[i] == '/') {
+            while (operatorsTop >= 0 && precedence(validUserInputs[i]) <= precedence(operators[operatorsTop])) {
+                int num2 = values[valuesTop--];
+                int num1 = values[valuesTop--];
+                char op = operators[operatorsTop--];
+                values[++valuesTop] = calculate(num1, num2, op);
             }
-            int lastNum = values[valuesTop--];
-            values[++valuesTop] = calculate(lastNum, num, op);
-        }
-        else if (validUserInputs[i] == '+' || validUserInputs[i] == '-') {
             operators[++operatorsTop] = validUserInputs[i];
             i++;
         }
@@ -59,6 +131,7 @@ int calculator(char* validUserInputs) {
             exit(1);
         }
     }
+
     while (operatorsTop >= 0) {
         int num2 = values[valuesTop--];
         int num1 = values[valuesTop--];
@@ -72,42 +145,18 @@ int calculator(char* validUserInputs) {
 int main() {
     char userInputs[VALID_SIZE];
     
-    printf("Enter a mathematical expression : ");
+    printf("Enter a mathematical expression: ");
     fgets(userInputs, VALID_SIZE, stdin);
-    for (int i = 0; i < VALID_SIZE; i++) {
-        if (userInputs[i] == '\n' || userInputs[i] == '\r') {
-            userInputs[i] = '\0';
-            break;
-        }
-    }
-    int len = strlen(userInputs);
-    int newIndex = 0;
 
-    for (int i = 0; i < len; i++) {
-        if (isspace(userInputs[i])) {
-            continue;
-        }
-        userInputs[newIndex++] = userInputs[i];
-    }
-    userInputs[newIndex] = '\0';
-    if (strlen(userInputs) == 0) {
-        printf("Error: Invalid expression.\n");
-        return 0;
-    }
-    for (int i = 0; i < strlen(userInputs); i++) {
-        if (!isdigit(userInputs[i]) && userInputs[i] != '+' && 
-            userInputs[i] != '-' && userInputs[i] != '*' && userInputs[i] != '/') {
-            printf("Error: Invalid expression.\n");
-            return 0;
-        }
-    }
+    remove_newline(userInputs);
+       
+    remove_spaces(userInputs);
 
-    int lastIndex = strlen(userInputs) - 1;
-    if (userInputs[lastIndex] == '+' || userInputs[lastIndex] == '-' || 
-        userInputs[lastIndex] == '*' || userInputs[lastIndex] == '/') {
-        userInputs[lastIndex] = '\0';
+    if (!validate_expression(userInputs)) {
+        return 0;  
     }
     int result = calculator(userInputs);
     printf("%d\n", result);
+
     return 0;
 }
